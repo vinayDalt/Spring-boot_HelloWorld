f05682e8b27b8c6862ca0ec8fa1bbdc46627dda7
vgu063 <Vinay.gupta1@maersk.com>
2020-08-12 18:23:14 +0530

    demo

---
+++ src/main/java/com/practice/SpringbootHelloWorld/Controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.practice.SpringbootHelloWorld.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String hello(){
+
+        return "Hello World";
+
+    }
+
+}
---
+++ src/main/java/com/practice/SpringbootHelloWorld/SpringBootHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.practice.SpringbootHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+
+}
